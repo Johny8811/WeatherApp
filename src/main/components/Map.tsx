453cf2ecfa09d5599fb017dc33a09885483743c0
Jan Hujsa <jan.hujsa@creativedock.com>
2022-02-08 09:34:38 +0100

    Improve

--- src/main/components/Map.tsx
+++ src/main/components/Map.tsx
@@ -1,4 +1,4 @@
-import { GoogleMap, Marker, withGoogleMap, withScriptjs } from "react-google-maps";
+import { GoogleMap, Marker, withGoogleMap, withScriptjs, GoogleMapProps } from "react-google-maps";
 import React from "react";
 
 import { useCoord, DEFAULT_COORDINATES } from '../../state/CoordProvider'
@@ -6,16 +6,18 @@ import { useCoord, DEFAULT_COORDINATES } from '../../state/CoordProvider'
 const MapComponent = () => {
   const { coord, setCoord } = useCoord();
 
+  const handleChangePosition: GoogleMapProps['onClick'] = ({ latLng }) => {
+    setCoord({
+      lat: latLng.lat(),
+      lon: latLng.lng()
+    })
+  }
+
   return (
     <GoogleMap
       defaultZoom={8}
       defaultCenter={{ lat: DEFAULT_COORDINATES.lat, lng: DEFAULT_COORDINATES.lon }}
-      onClick={({latLng}) => {
-        setCoord({
-          lat: latLng.lat(),
-          lon: latLng.lng()
-        })
-      }}
+      onClick={handleChangePosition}
     >
       {coord && (
         <Marker
